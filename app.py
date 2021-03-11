ff7424361dcb1ac91fab8db11d3c9b3980fa34a8
Geoffrey C. Hutchinson <geoffreyhutchinson018@gmail.com>
2021-03-11 13:51:16 +0000

    added password and login functionality on app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from flask import (
-    Flask, flash, render_template, 
+    Flask, flash, render_template,
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
@@ -25,7 +25,7 @@ mongo = PyMongo(app)
 def shares():
     share = mongo.db.shares.find()
     return render_template("share.html", share=share)
-    
+
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
@@ -47,6 +47,28 @@ def register():
         flash("Registration Successful!")
     return render_template("register.html")
 
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+       current_user = mongo.db.fan_users.find_one(
+           {"username": request.form.get("username").lower()})
+
+       if current_user:
+            if check_password_hash(
+                current_user["password"], request.form.get("password")):
+                    session["airlinefan"] = request.form.get("username").lower()
+                    flash("Welcome Aboard Airline Fans 101, {}!".format(
+                        request.form.get("username")))
+            else:
+                flash("Please return to the Boarding Gate! Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+
+       else: 
+            flash("Please return to the Boarding Gate! Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
