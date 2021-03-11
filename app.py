b84afe774717f5e7f8a8a05f27ee34d9cc7e9c6c
Geoffrey C. Hutchinson <geoffreyhutchinson018@gmail.com>
2021-03-11 01:12:53 +0000

    Adding a register.html folder and created a register template

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -25,6 +26,11 @@ def shares():
     share = mongo.db.shares.find()
     return render_template("share.html", share=share)
     
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
+
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
